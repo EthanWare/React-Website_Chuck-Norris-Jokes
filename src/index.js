dd21ea3d374bdc5a8db65bb704a4969031c5dcdc
Ethan Ware <ethan.ware@tcs.com>
2019-11-12 15:01:52 -0500

    Added a button that calls an api and sets the returned text to the state 'joke'

--- src/index.js
+++ src/index.js
@@ -2,32 +2,79 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Hello extends React.Component {
+const axios = require('axios');
+
+class Main extends React.Component {
+    
     constructor(){
         super();
         this.state = {
-            message: "my friend (from state)!"
-        }
-        this.updateMessage = this.updateMessage.bind(this);
+            fact: "",
+            categories: []
+        };
+        this.getFact = this.getFact.bind(this);
     }
 
-    updateMessage() {
-        this.setState({
-            message: "my friend (from changed state)!"
-        });
+    getFact(){
+        axios.get('https://api.chucknorris.io/jokes/random')
+            .then((response) => {
+                this.setState({fact: response.data.value})
+            });
     }
 
-    render() {
-        return (
+    // getCategories(){
+    //     axios.get('https://api.chucknorris.io/jokes/categories')
+    //         .then((response) => {
+    //             console.log(response)
+    //             //this.setState({categories: response.data.value})
+    //         });
+    // }
+
+    // componentWillMount(){
+    //     axios.get('https://api.chucknorris.io/jokes/categories')
+    //         .then((response) => {
+    //             console.log(response)
+    //             //this.setState({categories: response.data.value})
+    //         });
+    // }
+
+    render(){
+        return(
             <div>
-                <h1>Hello {this.state.message}!</h1>
-                <button onClick={this.updateMessage}>Click me!</button>
+                <h1>Chuck Noris Fact</h1>
+                <h3>{this.state.fact}</h3>
+                <button onClick={this.getFact}>Next Fact</button>
             </div>
         )
-    }  
+    }
 }
 
+// class Hello extends React.Component {
+//     constructor(){
+//         super();
+//         this.state = {
+//             message: "my friend (from state)!"
+//         }
+//         this.updateMessage = this.updateMessage.bind(this);
+//     }
+
+//     updateMessage() {
+//         this.setState({
+//             message: "my friend (from changed state)!"
+//         });
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Hello {this.state.message}!</h1>
+//                 <button onClick={this.updateMessage}>Click me!</button>
+//             </div>
+//         )
+//     }  
+// }
+
 ReactDOM.render(  
-    <Hello message="my friend" />,
+    <Main />,
     document.getElementById("root")
 );
