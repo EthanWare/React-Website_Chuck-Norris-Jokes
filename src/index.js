717b4f2568625ee4971cf14f6d160141738c7992
Ethan Ware <ethan.ware@tcs.com>
2019-11-13 10:18:41 -0500

    Added a drop-down menu to pick the catergory of fact to be displayed next

--- src/index.js
+++ src/index.js
@@ -7,43 +7,61 @@ const axios = require('axios');
 class Main extends React.Component {
     
     constructor(){
+
         super();
         this.state = {
             fact: "",
-            categories: []
-            // selectedOption: ""
+            categories: [],
+            selectedOption: "random"
         };
         this.getFact = this.getFact.bind(this);
         // this.componentDidMount = this.componentDidMount.bind(this)
+
+        // add random to the categories list
+        this.state.categories.push(<option value="random">random</option>);
+
+        this.getCategories();
+        this.getFact();
     }
 
     getFact(){
-        axios.get('https://api.chucknorris.io/jokes/random')
+        if(this.state.selectedOption === "random"){
+            axios.get('https://api.chucknorris.io/jokes/random')
             .then((response) => {
                 this.setState({fact: response.data.value})
             });
+        }
+        else{
+            axios.get('https://api.chucknorris.io/jokes/random?category=' + this.state.selectedOption)
+            .then((response) => {
+                this.setState({fact: response.data.value})
+            });
+        }
     }
 
-    //get categories at page load
-    // componentDidMount(){
-    //     axios.get('https://api.chucknorris.io/jokes/categories')
-    //         .then((response) => {
-    //             // console.log(response.data[1])
-    //             for(var i = 0;i < response.data.length;i++) {
-    //                 this.state.categories.push(<option value={response.data[i]}>{response.data[i]}</option>);
-    //             };
-    //         });
-    // }
+    getCategories(){
+        axios.get('https://api.chucknorris.io/jokes/categories')
+            .then((response) => {
+                for(var i = 0;i < response.data.length;i++) {
+                    this.state.categories.push(<option value={response.data[i]}>{response.data[i]}</option>);
+                };
+            });
+            // this.forceUpdate()
+    }
+
+    setSelectedOption(e){
+        this.setState({selectedOption: e.target.value});
+    }
 
     render(){
         return(
             <div className="main">
-                <h1>Chuck Noris Facts</h1>
+                <h1>Chuck Norris Facts</h1>
                 <h3>{this.state.fact}</h3>
                 <div>
-                    {/* <select>
+                    <select onChange={this.setSelectedOption.bind(this)}>
                         {this.state.categories}
-                    </select> */}
+                    </select>
                     <button onClick={this.getFact}>Next Fact</button>
                 </div>
             </div>
